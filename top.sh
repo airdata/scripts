2c2ff8c4a315233dddbcfe5fbf96fce5af2ce011
Rumen Lishkov <rumen.lishkov@resolve.io>
2020-01-29 11:31:12 +0200

    add top 5

--- top.sh
+++ top.sh
@@ -3,3 +3,6 @@ ps -eo pid,ppid,cmd,%mem,%cpu --sort=-%mem | head
 
 #Top 15 Processes by Memory Usage with ‘top’
 top -b -o +%MEM | head -n 22
+
+top 5
+ps -eo pmem,pcpu,vsize,pid,cmd | sort -k 1 -nr | head -5
