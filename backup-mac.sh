4b4ea80f3395458bbf27b73e53f85c714088fdf4
Rumen Lishkov <airdata@abv.bg>
2023-11-23 07:09:51 +0200

    add git create repos

--- backup-mac.sh
+++ backup-mac.sh
@@ -1,7 +1,18 @@
-rsync -auz /Volumes/store/GoPro/ /Volumes/store\ backup/GoPro/ 
-rsync -auz /Volumes/store/GoPro-sorted/ /Volumes/store\ backup/GoPro-sorted/
-#rsync -auz /Volumes/store/Studio/ /Volumes/store\ backup/Studio/ 
-rsync -auz /Volumes/store/Videos/ /Volumes/store\ backup/Videos/
-rsync -auz /Volumes/store/Docs/ /Volumes/store\ backup/Docs/
-rsync -auz /Volumes/store/Media/ /Volumes/store\ backup/Media/
+## DRY RUN
+## rsync -av --dry-run /Volumes/store/Studio/ /Volumes/store_backup/Studio/ | head --lines=-3 | tail --lines=+3
+
+#docs
+rsync -avh /Volumes/store/Docs/  /Volumes/store_backup/Docs/  #--delete
+
+#gopro
+rsync -avh /Volumes/store/GoPro/ /Volumes/store_backup/GoPro/ #--delete
+
+#media
+rsync -avh /Volumes/store/Media/  /Volumes/store_backup/Media/ #--delete
+
+#Studio
+rsync -avh /Volumes/store/Studio/ /Volumes/store_backup/Studio/ #--delete
+
+#videos
+rsync -avh /Volumes/store/Videos/ /Volumes/store_backup/Videos/ #--delete
 
